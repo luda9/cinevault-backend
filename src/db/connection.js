14b7e34c7218a185a4c07013927344bb383490ef
Daniel Santillan <103224551+luda9@users.noreply.github.com>
2026-01-30 23:31:56 -0600

    database modified for hosting

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,13 +1,14 @@
-const mysql = require('mysql2/promise')
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'cinevault',
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
-})
+  queueLimit: 0,
+});
 
-module.exports = pool
+module.exports = pool;
